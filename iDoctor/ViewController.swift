f70061d15c2773b257a2ad036b774fceb737214c
mohammed <mohammederbia98@gmail.com>
2020-05-22 03:00:14 +0300

    Initial Commit

---
+++ iDoctor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iDoctor
+//
+//  Created by mohammed on 5/22/20.
+//  Copyright © 2020 iDoctor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iDoctorTests/iDoctorTests.swift
@@ -0,0 +1,34 @@
+//
+//  iDoctorTests.swift
+//  iDoctorTests
+//
+//  Created by mohammed on 5/22/20.
+//  Copyright © 2020 iDoctor. All rights reserved.
+//
+
+import XCTest
+@testable import iDoctor
+
+class iDoctorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
