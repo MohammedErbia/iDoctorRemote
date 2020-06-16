bbc9173ea464e9ae1d52e295dac88a278f354dce
mohammed <mohammederbia98@gmail.com>
2020-06-16 13:19:56 +0300

    add some view conttrolur

---
+++ iDoctor/Storyborad/Main/NotificationViewController.swift
@@ -0,0 +1,34 @@
+//
+//  NotificationViewController.swift
+//  iDoctor
+//
+//  Created by mohammed on 6/16/20.
+//  Copyright © 2020 iDoctor. All rights reserved.
+//
+
+import UIKit
+
+class NotificationViewController: UIViewController {
+     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         
+}
+    
+    
+} 
+extension NotificationViewController : UITableViewDataSource,UITableViewDelegate{
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
+
+
+}
+ 
---
+++ iDoctor/Storyborad/Main/searchViewController.swift
@@ -0,0 +1,52 @@
+//
+//  searchViewController.swift
+//  iDoctor
+//
+//  Created by mohammed on 6/16/20.
+//  Copyright © 2020 iDoctor. All rights reserved.
+//
+
+import UIKit
+
+class searchViewController: UIViewController {
+     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        search ()
+}
+    func search () {
+    let searchController = UISearchController(searchResultsController: nil)
+    searchController.searchResultsUpdater = self
+    searchController.obscuresBackgroundDuringPresentation = true
+    searchController.searchBar.placeholder = "البحث"
+        searchController.searchBar.setPlaceholder(textColor: .gray)
+        searchController.searchBar.set(textColor: #colorLiteral(red: 0, green: 0.8666666667, blue: 0.4470588235, alpha: 1))
+        searchController.searchBar.setTextField(color: UIColor.white)
+        searchController.searchBar.setSearchImage(color: .gray)
+    searchController.searchBar.tintColor = .white
+    
+    searchController.searchBar.barTintColor = .white
+        searchController.searchBar.borderColor = .white
+    self.navigationItem.searchController = searchController
+    self.definesPresentationContext = true
+
+    }
+    
+}
+ extension searchViewController: UISearchResultsUpdating {
+
+ func updateSearchResults(for searchController: UISearchController) {
+     print("Searching with: " + (searchController.searchBar.text ?? ""))
+     let searchText = (searchController.searchBar.text ?? "")
+//     guard searchText.count > 2 && !searchText.isEmpty else {
+//         return
+//     }
+//     let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SearchVC") as! SearchVC
+//     vc.Key = searchText
+//     self.navigationController?.pushViewController(vc, animated: true)
+     searchController.searchBar.text = ""
+     }
+     
+ }
+ 
